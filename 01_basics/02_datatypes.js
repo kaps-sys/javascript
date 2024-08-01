02421a35da8a3718aba018221223a6de0fa583e2
kaps-sys <gehlotk691@gmail.com>
2024-08-01 14:10:06 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict" // use js code for newer version
+//alert(6+6)
+//datatypes
+
+let age=6;
+let name="kapil"
+// number
+//string=""
+//boolean=true/false
+//null=standalone value
+//undefined
+//symbol= used for uniqueness
+
+console.log(typeof "hitesh")
+console.log(typeof (null))
