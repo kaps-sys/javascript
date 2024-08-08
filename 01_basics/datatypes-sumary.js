6b9d15bee8cb214ea47b27a4706f2909a1aed55e
kaps-sys <gehlotk691@gmail.com>
2024-08-08 14:55:13 +0000

    summarydatatypes

---
+++ 01_basics/datatypes-sumary.js
@@ -0,0 +1,40 @@
+// primitive
+
+// 7 types:string,int,null,object,undefined,symbol,bigint
+
+
+const score=100
+const isloggedin=true
+const temp=null
+let x
+const id=Symbol('123')
+const newid=Symbol('123')
+//console.log(id==newid)
+
+// non primitive(reference )
+// array,objects,function
+
+let fruits=['apple','mango']
+let obj={
+    name:'kapil',
+    age:22
+}
+const myfunction=function(){
+    console.log('helo word')
+}
+
+//datatype return type
+//int number
+//boolean true or false
+//bigint undefined
+//null object
+//string string
+//undefined undeined 
+//symbol symbl
+//object object
+//array obj
+
+
+console.log(typeof fruits)
+
+//js is dynamically typed language bcz datatype is automatically assigned at compilation or execution of  code 
