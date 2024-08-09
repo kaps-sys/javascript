9c860cf5ad96a7ab68e84e83d6871451b237e859
kaps-sys <gehlotk691@gmail.com>
2024-08-09 09:17:23 +0000

    extra

--- 01_basics/datatypes-sumary.js
+++ 01_basics/datatypes-sumary.js
@@ -35,6 +35,26 @@ const myfunction=function(){
 //array obj
 
 
-console.log(typeof fruits)
+//console.log(typeof fruits)
 
 //js is dynamically typed language bcz datatype is automatically assigned at compilation or execution of  code 
+
+//###########################################
+
+//Stack(primitive),Heap(non primitive)
+let myname='kapil gehlot'
+newname=myname
+newname='ram'
+//console.log(newname)
+//console.log(newname)
+//in stack copy of variable value is stored in an another variable
+
+let user1={
+    name:'kapil',
+    id:101
+}
+let user2=user1
+user2.name='ram';
+console.log(user1)
+console.log(user2)
+//in heap sort 2nd variable takes reference of 1st variable and only value changes in 1st variable
